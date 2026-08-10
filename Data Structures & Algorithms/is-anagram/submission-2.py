91fdd72bf596cbba0e342a3a91c26cb110c5faf4
Lg_codes <laurengoff99883@gmail.com>
2026-08-09 23:15:43 -0500

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        hash_map1 = {}
+        hash_map2 = {}
+
+        for i in s:
+
+            if i in hash_map1:
+                hash_map1[i] += 1
+            else:
+                hash_map1[i] = 1
+
+        for j in t:
+
+            if j in hash_map2:
+                hash_map2[j] += 1
+            else:
+                hash_map2[j] = 1
+        
+
+        return hash_map1 == hash_map2
